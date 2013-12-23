371769469844e564bef15555454645da8f9a1cae
splitice <mat999@gmail.com>
2013-12-23 12:00:25 +1030

    Webgrind move

---
+++ src/Radical/Core/Debug/WebGrind/Handler.php
@@ -0,0 +1,23 @@
+<?php
+namespace Radical\Core\Debug\WebGrind;
+
+use Web\Template;
+use Web\Page\Handler\PageBase;
+
+class Handler extends PageBase {
+	protected $template;
+	protected $filename;
+	
+	function __construct($template,$filename){
+		$this->template = $template;
+		$this->filename = $filename;
+	}
+	function GET(){
+		$template = new Template($this->template,array(),'webgrind');
+		$template->vars['filename'] = $this->filename;
+		return $template;
+	}
+	function POST(){
+		return $this->GET();
+	}
+}
